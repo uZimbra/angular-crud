1aaea179d97472489d8df8233e589752b061c895
Rodrigo A. Silva <rgbrasil2@gmail.com>
2022-02-11 11:31:08 -0300

    chore: Lista de Cursos: Tratamento de Erros e MatDialog

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, Observable, of } from 'rxjs';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
@@ -13,8 +15,19 @@ export class CoursesComponent implements OnInit {
   courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor(private service: CoursesService) {
-    this.courses$ = this.service.list();
+  constructor(private service: CoursesService, public dialog: MatDialog) {
+    this.courses$ = this.service.list().pipe(
+      catchError((err) => {
+        this.onError('Ocorreu uma falha inesperado para carregar os dados!');
+        return of([]);
+      })
+    );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg,
+    });
   }
 
   ngOnInit(): void {}
